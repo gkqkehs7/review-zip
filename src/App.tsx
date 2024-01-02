95fa42b16edad10e288a77789c167cf38b801cf4
gkqkehs7 <77993709+gkqkehs7@users.noreply.github.com>
2024-01-02 20:37:23 +0900

    Setting: test github desktop

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <Button>sss</Button>
+      <Button>Test github desktop</Button>
     </>
   );
 }
