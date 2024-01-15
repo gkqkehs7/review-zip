87d2b4d4d3cf13cb8e833b98c5aa4d3669d10003
gkqkehs7 <77993709+gkqkehs7@users.noreply.github.com>
2024-01-15 19:53:29 +0900

    Feature: add search logic

--- src/components/common/likeListComponent/likeListComponent.tsx
+++ src/components/common/likeListComponent/likeListComponent.tsx
@@ -1,5 +1,10 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { faker } from '@faker-js/faker';
-import React, { useEffect, useRef } from 'react';
+
+import { changeInputValue } from '@/hooks/chageInputValue';
+
+import { IUser } from '@/types/users.types';
+
 import styles from './style';
 
 interface LikeListComponentProps {
@@ -7,50 +12,57 @@ interface LikeListComponentProps {
   likeListOpen: boolean;
 }
 
-const users = [
+const users: IUser[] = [
   {
     id: 1,
     name: '제니',
+    nickname: '닉넴1',
     profileImage: faker.image.avatar(),
     follow: false,
   },
 
   {
-    id: 1,
-    name: '제니',
+    id: 2,
+    name: '예니',
+    nickname: '닉넴1',
     profileImage: faker.image.avatar(),
     follow: false,
   },
 
   {
-    id: 1,
-    name: '제니',
+    id: 3,
+    name: '민우',
+    nickname: '닉넴1',
     profileImage: faker.image.avatar(),
     follow: false,
   },
 
   {
-    id: 1,
-    name: '제니',
+    id: 4,
+    name: '나이',
+    nickname: '닉넴1',
     profileImage: faker.image.avatar(),
     follow: false,
   },
 
   {
-    id: 1,
-    name: '제니',
+    id: 5,
+    name: '거미',
+    nickname: '닉넴1',
     profileImage: faker.image.avatar(),
     follow: false,
   },
   {
-    id: 1,
-    name: '제니',
+    id: 6,
+    name: '코끼리',
+    nickname: '닉넴1',
     profileImage: faker.image.avatar(),
     follow: false,
   },
   {
-    id: 1,
-    name: '제니',
+    id: 7,
+    name: '미누',
+    nickname: '닉넴1',
     profileImage: faker.image.avatar(),
     follow: false,
   },
@@ -60,7 +72,35 @@ const LikeListComponent: React.FC<LikeListComponentProps> = ({
   closeLikeList,
   likeListOpen,
 }) => {
+  const [searchInput, setSearchInput] = useState<string>('');
+
   const userList = useRef<HTMLDivElement>(null);
+
+  const filterUsers = useCallback(
+    (users: IUser[]) => {
+      const filterUserlist = users.filter((user: IUser) =>
+        user.name.includes(searchInput),
+      );
+
+      return (
+        <>
+          {filterUserlist.map((user: IUser) => (
+            <styles.UserList key={user.id}>
+              <styles.UserData>
+                <styles.UserImage src={user.profileImage} />
+                <styles.UserName>{user.name}</styles.UserName>
+              </styles.UserData>
+
+              <styles.FollowButton>
+                <styles.FollowText>삭제</styles.FollowText>
+              </styles.FollowButton>
+            </styles.UserList>
+          ))}
+        </>
+      );
+    },
+    [searchInput],
+  );
 
   // 유저 리스트에 focus되도록 하는 함수
   useEffect(() => {
@@ -90,7 +130,10 @@ const LikeListComponent: React.FC<LikeListComponentProps> = ({
 
         <styles.SearchContainer>
           <styles.SearchIcon>돋보기 그림</styles.SearchIcon>
-          <styles.SearchInput placeholder="검색" />
+          <styles.SearchInput
+            placeholder="검색"
+            onChange={(e) => changeInputValue(e, setSearchInput)}
+          />
         </styles.SearchContainer>
 
         <styles.UserListContainer
@@ -98,18 +141,7 @@ const LikeListComponent: React.FC<LikeListComponentProps> = ({
           onScroll={(e) => e.preventDefault()}
           onWheel={(e) => e.preventDefault()}
         >
-          {users.map((user, index) => (
-            <styles.UserList key={index}>
-              <styles.UserData>
-                <styles.UserImage src={user.profileImage} />
-                <styles.UserName>{user.name}</styles.UserName>
-              </styles.UserData>
-
-              <styles.FollowButton>
-                <styles.FollowText>삭제</styles.FollowText>
-              </styles.FollowButton>
-            </styles.UserList>
-          ))}
+          {filterUsers(users)}
         </styles.UserListContainer>
       </styles.InnerContainer>
     </styles.Container>
---
+++ src/types/users.types.ts
@@ -0,0 +1,9 @@
+type IUser = {
+  id: number;
+  name: string;
+  nickname: string;
+  profileImage: string;
+  follow: boolean;
+};
+
+export type { IUser };
