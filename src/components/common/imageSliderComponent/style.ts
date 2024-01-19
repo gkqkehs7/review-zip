2fd137e10a440484c93b9b9d422009d55b0450bd
gkqkehs7 <77993709+gkqkehs7@users.noreply.github.com>
2024-01-19 20:03:21 +0900

    Refactor: mainpage

--- src/components/common/imageSliderComponent/style.ts
+++ src/components/common/imageSliderComponent/style.ts
@@ -7,6 +7,7 @@ const styles = {
     flex: 1;
     width: 100%;
     margin: 10px 0;
+    position: relative;
   `,
 
   ImageContainer: styled(SwiperSlide)`
@@ -19,6 +20,17 @@ const styles = {
   Image: styled.img`
     width: 100%;
     object-fit: cover;
+  `,
+
+  NextButton: styled.img`
+    width: 25px;
+    height: 25px;
+
+    position: absolute;
+    top: 50%;
+    right: 0;
+
+    z-index: 9;
   `,
 };
 
