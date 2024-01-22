05e6a6655dca2452e66192090efa3282634fe75f
chae-eunshin <115004531+chae-eunshin@users.noreply.github.com>
2024-01-22 17:51:51 +0900

    Featur: hashtagPage

--- src/components/searchPageComponent/hashtagComponent/hashtagComponent.tsx
+++ src/components/searchPageComponent/hashtagComponent/hashtagComponent.tsx
@@ -1,6 +1,6 @@
 import { responsiveWidthHeight } from '@/utils/reponsiveSize';
 import { checkDevice } from '@/utils/checkDeviceSize';
-
+import { Link } from 'react-router-dom';
 import styles from './style';
 
 interface HashtagComponentProps {
@@ -18,19 +18,21 @@ const HashtagComponent: React.FC<HashtagComponentProps> = ({
     //해시태그인 경우
     <styles.HistoryContainer>
       <styles.HashtagContainer>
-        <styles.HashtagImage
-          src={searchWordImage}
-          style={responsiveWidthHeight(
-            device,
-            { width: 56, height: 53 },
-            { width: 56, height: 53 },
-            { width: 56, height: 53 },
-            { width: 36, height: 33 },
-            { width: 16, height: 10 },
-            { width: 16, height: 10 },
-          )}
-        />
-        <styles.Content>{value}</styles.Content>
+        <Link to="/hashtagPage">
+          <styles.HashtagImage
+            src={searchWordImage}
+            style={responsiveWidthHeight(
+              device,
+              { width: 56, height: 53 },
+              { width: 56, height: 53 },
+              { width: 56, height: 53 },
+              { width: 36, height: 33 },
+              { width: 16, height: 10 },
+              { width: 16, height: 10 },
+            )}
+          />
+          <styles.Content>{value}</styles.Content>
+        </Link>
       </styles.HashtagContainer>
       <styles.SeeReviewContainer>
         <styles.SeeReview to="">리뷰 보기</styles.SeeReview>
