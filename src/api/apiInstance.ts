394605701be6983f03ac9d16552ee6de2bde414f
gkqkehs7 <77993709+gkqkehs7@users.noreply.github.com>
2024-02-06 01:32:39 +0900

    Fix: split base url

--- src/api/apiInstance.ts
+++ src/api/apiInstance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.PROD ? 'https://api.egusajo.shop' : '/api',
   withCredentials: true,
 });
 
